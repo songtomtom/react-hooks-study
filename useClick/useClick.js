edec5e429868ede2fc8247144511a9cdf9ef3f11
tom.song <tom.song@luxrobo.com>
2020-08-02 19:48:38 +0900

    Create to 'useTitle'

---
+++ useClick/useClick.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+export default useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTlte = document.querySelector('title');
+    htmlTlte.innerText = title;
+  };
+
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
+
+// export default function App() {
+//   const titleUpdater = useTitle('Loading...');
+//   setTimeout(() => {
+//     titleUpdater('Home');
+//   }, 5000);
+//   return (
+//     <div className="App">
+//       <div>Hi</div>
+//     </div>
+//   );
+// }
